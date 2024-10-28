860da2934946baff1a4e4b9c331a2ad441f054e1
dfbwy <dfbwy233@qq.com>
2024-10-28 11:58:08 +0800

    html_theme = 'traditional'  extensions = ['recommonmark']

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['recommonmark']
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+html_theme = 'traditional'
 html_static_path = ['_static']
